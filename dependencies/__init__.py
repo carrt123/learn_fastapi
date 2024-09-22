3409b5df1ab2be10693999bbfb3c735d07222b8e
carrt123 <sam@globadevops.io>
2024-09-23 00:35:13 +0800

    依赖

---
+++ dependencies/__init__.py
@@ -0,0 +1,20 @@
+from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.ext.asyncio import AsyncSession
+from typing import AsyncGenerator
+from db.database import AsyncSessionLocal
+from contextlib import asynccontextmanager
+
+
+async def get_db_session() -> AsyncGenerator[AsyncSession, None]:
+    db_session = None
+    try:
+        db_session = AsyncSessionLocal()
+        yield db_session
+    finally:
+        await db_session.close()
+
+
+@asynccontextmanager
+async def get_async_session() -> AsyncSession:
+    async with AsyncSessionLocal() as session:
+        yield session
